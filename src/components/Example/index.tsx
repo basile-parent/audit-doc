b5d7db7f65adbfa70c0cb71ad06968000b7639bf
Basile Parent <casou.acc@gmail.com>
2025-06-15 11:18:15 +0200

    Theme 6

--- src/components/Example/index.tsx
+++ src/components/Example/index.tsx
@@ -1,22 +1,45 @@
 import {HTMLAttributes, JSXElementConstructor} from "react";
 import styles from "./styles.module.css"
 
-type ExampleProps = HTMLAttributes<HTMLImageElement> & {
-    imgImport: string
-    comply?: boolean
-    special?: boolean
+type Special = {
+    special?: true
+    comply?: never
 }
+type Comply = {
+    comply?: true
+    special?: never
+}
+type ResultText = Special | Comply
+
+type ExampleProps = HTMLAttributes<HTMLImageElement> & {
+    imgImport?: string
+} & ResultText
 const Example = ({className, imgImport, children, comply, special, ...otherProps}: ExampleProps) => {
+    if (!imgImport) {
+        return (
+            <div className={`${styles.wrapper} ${className ?? ""}`} {...otherProps}>
+                {
+                    special ?
+                        <span className={styles.special}>Cas particulier</span> :
+                        comply ?
+                            <p className={styles.compliance}><span aria-hidden>✓</span> Conforme</p> :
+                            <p className={styles.nonCompliance}><span aria-hidden>X</span> Non conforme</p>
+                }
+                {children}
+            </div>
+        )
+    }
+
     return (
-        <figure className={`${styles.figure} ${className ?? ""}`} {...otherProps}>
+        <figure className={`${styles.wrapper} ${styles.figure} ${className ?? ""}`} {...otherProps}>
             <img src={imgImport} alt="Image d'exemple"/>
             <figcaption>
                 {
                     special ?
                         <span className={styles.special}>Cas particulier</span> :
                     comply ?
-                        <span className={styles.compliance}><span aria-hidden>✓</span> Conforme</span> :
-                        <span className={styles.nonCompliance}><span aria-hidden>X</span> Non conforme</span>
+                        <p className={styles.compliance}><span aria-hidden>✓</span> Conforme</p> :
+                        <p className={styles.nonCompliance}><span aria-hidden>X</span> Non conforme</p>
                 }
                 {children}
             </figcaption>
